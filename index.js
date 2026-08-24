d250ba01bbd70f835096a3bff400359c381c9226
Tony <diyorbeksolijonov27@gmail.com>
2026-08-24 10:00:02 +0900

    fix: try, catch, throw

--- index.js
+++ index.js
@@ -84,8 +84,7 @@ for(let i =1; i<=10; i++){
 
 letterFinder("test", "t")*/
 
-let greet = "Hello";
-let user = "John";
+
 // console.log(greet.pop())
 // console.log(greet.concat(user));  //The concat() method joins two strings: 
 // console.log(greet + " " + user);
@@ -93,7 +92,7 @@ let user = "John";
 // console.log(greet.charAt(3)); // returns the character at index 3
 // console.log("Wo".concat("rl").concat("d")); // 'World'
 // console.log("ho-ho-ho".split("-")); // ['ho', 'ho', 'ho'] The split method chops up the string into an array of sub-strings:
-
+/*
 let clothes =[];
 clothes.push("T-shirt");
 clothes.push("Jeans");
@@ -107,4 +106,100 @@ console.log(clothes);
 clothes.push("Hat"); // adds an item to the end of the array
 console.log(clothes);
 clothes.unshift("Shoes"); // adds an item to the beginning of the array
+console.log("==========");
+
+let favCar = {};
+favCar.color = "red";
+favCar.convertible = true;
+console.log(favCar);
+*/
+
+// #region Old Practice Code
+/*car.color = "blue";
+car.convertible = false;
+car.turnTheKey = function(){
+    console.log("You turn the key and the car starts.");
+}
+car.lightsOn = function(){
+    console.log("You turn the lights on.");
+}
+console.log(car);
+car.turnTheKey();
+car.lightsOn();
+//typeOf operator that evaluates a parameter and returns data types as a string
+let test = typeof(23>12);
+console.log(test); // object*/
+// let greet = "Hello";
+// let user = "John";
+// let car = {};
+console.log("==========");
+
+// function myFunc(a,b) {
+//     c= a+b
+//     console.log(c);
+// }
+
+// myFunc(5,10);
+
+//Error handling using try and catch
+
+// try{
+//     console.log(a+b)
+// }catch(err){
+//  console.log(err.message)
+//  console.log("Error occured");
+//  console.log("The error was saved in the eror log")
+// }
+// console.log("My program does not stop here because the error is handled.");
+
+// try{
+//     throw new ReferenceError();
+// }catch(err){
+//  console.log(err.stack)
+//  console.log("There was a Reference Error");
+// }
+// console.log("My program does not stop here because the error is handled.");
+
+// let letters = "abc";
+// console.log(letters.match(/a/));
+// let ol
+// console.log(ol)
+// ol = "thunder"
+// console.log(ol)
+
+
+// function addTwoNums(a,b) {
+//    try{
+//     if (typeof a === "number" && typeof b === "number"){
+//      console.log(a + b);
+//      }
+//    }catch(err){
+//     console.log("Error caught:", err);
+//    }
+// }
+
+// function addTwoNums(a,b) {
+//     if (typeof a !== "number" || typeof b !== "number") 
+//     {throw new TypeError("Both arguments must be numbers");
+
+//     }else{
+//         console.log(a + b);}
+// }
+// addTwoNums("5", 5);
+
+function addTwoNums(a,b) {
+    try {
+        if(typeof(a) != 'number') {
+            throw new ReferenceError('the first argument is not a number')
+        } else if (typeof(b) != 'number') {
+            throw new ReferenceError('the second argument is not a number')
+        } else {
+            console.log(a + b)
+        }
+    } catch(err) {
+        console.log("Error!", err)
+    }
+}
+addTwoNums(5, 1)
+console.log("It still works")
 console.log("==========");
